fa787b74a2cdbcc4b0154a5f914d10df0e19a29f
yasutakah <h-yasu@umin.ac.jp>
2016-06-11 11:00:53 +0900

    Update cachematrix.r

--- cachematrix.r
+++ cachematrix.r
@@ -1,17 +1,7 @@
-## cachematrix.R
-##
-## Create a cache marix object that can be used to
-## repeatably solve the inverse of the marix, but only
-## calculates the inverse once.
-##
-## Usage:
-##  M <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+##  Cachematrix.R
+##  M :Matrix
 ##  cacheMatrix <- makeCacheMatrix(M)
 ##  cacheSolve(cacheMatrix)
-##
-##  cacheMatrix$set(M)      # Change the matrix being cached.
-##  M <- cacheMatrix$get()  # Returns the matrix being cached.
-##
 ##  cacheMatrix$setInverse(solve(data, ...)) # Private function containing cached inverse of x
 ##  cacheMatrix$getInverse()                 # Private function used to get the cached inverse of x
 
@@ -35,12 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Return the inverse of an cacheMatrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        invFunc <- x$getInverse()
-        if(!is.null(invFunc)) {
+        inverseFunc <- x$getInverse()
+        if(!is.null(inverseFunc)) {
                 message("getting cached data")
-                return(invFunc)
+                return(inverseFunc)
         }
+        data <- x$get()
+        inverseFunc <- solve(data, ...)
+        x$setInverse(inverseFunc)
+        inverseFunc
+}
         data <- x$get()
         invFunc <- solve(data, ...)
         x$setInverse(invFunc)
